469550cfaa748b909c0fa0c37c91092e937e6d94
itabr <itabrz@gmail.com>
2017-10-24 12:01:05 -0700

    index website

---
+++ part-2/UID/sql/www/index.php
@@ -0,0 +1,101 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>CS143 DataBase</title>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<nav class="navbar navbar-dark bg-dark">
+		<a class="navbar-brand" href="#"> CS143 DataBase Query System </a>
+		<a class="navbar-brand" style="color: #C0C0C0;"> <b>Ilya Tabriz &nbsp;&nbsp; and &nbsp;&nbsp; Therese Horey</b> </a>
+	</nav>
+
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col" style="margin: 10px 0px 10px 0px;">
+				<a>
+				Add New Content
+				</a>
+			</div>
+			<div class="col" style="margin: 10px 0px 10px 0px;">
+				<a>
+				Browsering Content
+				</a>
+			</div>
+			<div class="col" style="margin: 10px 0px 10px 0px;">
+				<a>
+				Search Interface
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+			   Add Actor/Director
+			   </a>
+			</div>
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+				Show Actor Information
+				</a>
+			</div>
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+				Search/Actor Movie
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+			   Add Movie Information
+			   </a>
+			</div>
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+				Show Movie Information
+				</a>
+			</div>
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+			   Add Movie/Actor Relation
+			   </a>
+			</div>
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+				</a>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col" style="margin: 5px 0px 5px 0px;">
+				<a href="#">
+					Add Movie/Director Relation
+			   </a>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+
+</body>
+
+
+</html>
